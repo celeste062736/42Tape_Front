183b49637d13fae01c8232d40f2d179c9e32d1a1
celeste062736 <skykimmg0627@gmail.com>
2023-08-25 16:27:50 +0900

    add

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,10 +1,24 @@
-import "next-auth/jwt"
+// import "next-auth/jwt"
 
+import { DefaultSession, DefaultUser } from "next-auth";
+
+export enum Role {
+  admin = "admin",
+  user = "user",
+}
 // Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
 
-declare module "next-auth/jwt" {
-  interface JWT {
-    /** The user's role. */
-    userRole?: "admin" | "user"
+interface IUser extends DefaultUser {
+  role?: Role;
+}
+
+declare module "next-auth" {
+  interface User extends IUser {}
+  interface Session {
+    user?: User,
   }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends IUser {}
 }
