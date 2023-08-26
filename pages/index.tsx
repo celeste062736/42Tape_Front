28eb8c219995e85fd447816e1634da07bda0f6d6
leeesooha <suha630@naver.com>
2023-08-26 12:00:03 +0900

    broken code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,40 @@
-import Layout from "../components/layout"
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import Home from './home'
+// import type { UserInfo, ProfileProps, RadarChartProps, LineChartProps, MainLayoutProps} from "../components/temp_components";
 
-export default function IndexPage() {
-  return (
-    <Layout>
-      <h1>NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
-      </p>
-    </Layout>
-  )
-}
+
+// type IndexPage = {
+//   _userInfo: UserInfo
+// };
+
+// export const metadata = {
+//   title: '42TAPE',
+//   description: '42 The Art of Peer Evaluation',
+// }
+
+// export default function IndexPage(props: IndexPage) {
+//   let { _userInfo } = props;
+//   return (
+//     <Home userInfo={_userInfo}></Home>
+//   )
+// }
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html>
+//       <body>{children}</body>
+//     </html>
+//   )
+// }
+
+// export default function IndexPage() {
+//   return (
+//     <Layout>
+//       <h1>NextAuth.js Example</h1>
+//       <p>
+//         This is an example site to demonstrate how to use{" "}
+//         <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
+//       </p>
+//     </Layout>
+//   )
+// }
