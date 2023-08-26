012944dbfa612a67bec241247684bdcab911ad73
celeste062736 <skykimmg0627@gmail.com>
2023-08-26 21:48:12 +0900

    Add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Layout from "../components/layout"
 import { MainLayout, TopBar } from '../components/Components'
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
@@ -71,45 +70,3 @@ export const getServerSideProps: GetServerSideProps<{
   }
   return { props: {userInfo: data}}
 }
-=======
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import Home from './home'
-// import type { UserInfo, ProfileProps, RadarChartProps, LineChartProps, MainLayoutProps} from "../components/temp_components";
-
-
-// type IndexPage = {
-//   _userInfo: UserInfo
-// };
-
-// export const metadata = {
-//   title: '42TAPE',
-//   description: '42 The Art of Peer Evaluation',
-// }
-
-// export default function IndexPage(props: IndexPage) {
-//   let { _userInfo } = props;
-//   return (
-//     <Home userInfo={_userInfo}></Home>
-//   )
-// }
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html>
-//       <body>{children}</body>
-//     </html>
-//   )
-// }
-
-// export default function IndexPage() {
-//   return (
-//     <Layout>
-//       <h1>NextAuth.js Example</h1>
-//       <p>
-//         This is an example site to demonstrate how to use{" "}
-//         <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
-//       </p>
-//     </Layout>
-//   )
-// }
->>>>>>> 28eb8c219995e85fd447816e1634da07bda0f6d6
