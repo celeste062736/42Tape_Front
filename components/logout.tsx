390a9aef44b045222c0014619525fd09d15e50d9
woosmile <106380619+woosmile@users.noreply.github.com>
2023-09-04 13:30:41 +0900

    update topbar icon

--- components/logout.tsx
+++ components/logout.tsx
@@ -2,7 +2,7 @@
 import { signOut } from "next-auth/react";
 
 export function LogoutButton() {
-    return <button id="logout_btn" onClick={(e) => {
+    return <button id="logout_btn" style={{padding: '0px'}} onClick={(e) => {
         signOut()
     }}>LOGOUT</button>;
 }
